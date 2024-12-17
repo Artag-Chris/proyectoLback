2628467041d54393208e5aaf1eaafce02377a981
Artag/Chris <scristxyz@gmail.com>
2024-12-17 11:09:57 -0500

    act

--- src/usuarios/usuario.controller.ts
+++ src/usuarios/usuario.controller.ts
@@ -1,15 +1,21 @@
 import { Controller,Get, Logger } from "@nestjs/common";
+import { UsuariosService } from "./usuarios.service";
 
 @Controller('usuarios')
 export class UsuariosController {
-constructor(){
+constructor(
+private readonly usuariosService: UsuariosService
+){
     const logger = new Logger('UsuariosController');
+    
 }
 
 
     @Get('/clientes')
+
     getUsuarios(){
         
+       const respuesta = this.usuariosService.getUsuarios();
         return "getUsuarios";
     }  
 
--- src/usuarios/usuarios.service.ts
+++ src/usuarios/usuarios.service.ts
@@ -1,10 +1,59 @@
-import { Injectable } from "@nestjs/common";
-import { get } from "http";
+import { Injectable, Logger, OnModuleInit } from "@nestjs/common";
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class UsuariosService {
+export class UsuariosService extends PrismaClient implements OnModuleInit {
+
+  private readonly logger = new Logger('ProductsService');
+
+
+  onModuleInit() {
+    this.$connect();
+    this.logger.log('Database connected');
+  }
+
+
+  async getUsuarios() {
+
+    try {
+      const usuarios = await this.user.findMany();
+      return usuarios;
+    } catch (error) {
+      this.logger.error(`Error getting users: ${error.message}`);
+      throw new Error('Error getting users');
+    }
+   
+  }
+
+  async getUsuario() {
+    //usa un try catch
+    try {
+      const usuario = await this.user.findUnique({ where: { id: 1 } });
+      return usuario;
+    } catch (error) {
+      this.logger.error(`Error getting user: ${error.message}`);
+      throw new Error('Error getting user');
+    }
     
-    constructor(){}
+  }
+
+  async createUsuario() {
+    /* crea un nuevo usuario en la base de datos */
+
+    return "createUsuario";
+  }
+  async updateUsuario() {
+    /* actualiza un usuario en la base de datos */  
+
+    return "updateUsuario";
+  }
+
+  async deleteUsuario() {
+    /* elimina un usuario de la base de datos */  
+
+    return "deleteUsuario";
+  }
 
   
+
 }
