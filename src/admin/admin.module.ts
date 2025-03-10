350b1d19966d1c58d1b6be6215f09a677246248c
Artag/Chris <scristxyz@gmail.com>
2025-03-10 14:11:17 -0500

    act exportacion de modulos

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AdminController } from './admin.controller';
 import { AdminService } from './admin.service';
+import { PedidosModule } from 'src/pedidos/pedidos.module';
+import { ProductosModule } from 'src/productos/productos.module';
+import { UsuariosModule } from 'src/usuarios/usuarios.module';
 
 @Module({
     controllers: [AdminController],
     providers: [AdminService],
+    imports: [UsuariosModule, 
+        ProductosModule, 
+        PedidosModule],
 })
 export class AdminModule {}
